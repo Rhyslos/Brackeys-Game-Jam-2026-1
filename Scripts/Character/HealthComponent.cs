78ff7fab6235d08b1efe7ae8e711fa050a4fc78c
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-17 10:51:43 +0100

    HealthComponent Init
    Why not learn composition while we're at it

---
+++ Scripts/Character/HealthComponent.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class HealthComponent : Node
+{
+	[Export] public int HealthAmount = 10;
+
+    public override void _Ready()
+    {
+        
+    }
+
+
+	
+}
