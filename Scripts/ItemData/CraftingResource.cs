e4120bc62e3f383468c852f3ff24d34ee89b3e10
Yuna Emiya <yuna-emiya@outlook.com>
2026-02-18 16:53:42 +0100

    --

--- Scripts/ItemData/CraftingResource.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using System;
-
-[GlobalClass]
-public partial class CraftingResource : Resource
-{
-    [Export] public new string ResourceName { get; set; }
-    [Export] public string ResourceID { get; set; }
-}
